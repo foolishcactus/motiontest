16827d421026e286cfac8cfb6a00c5f447b2fec7
Eshaan Gumudavelli <egumudavelli@gmail.com>
2023-08-26 15:08:33 -0400

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,73 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { Motion } from '@capacitor/motion';
+import { PluginListenerHandle } from '@capacitor/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule,CommonModule],
 })
 export class HomePage {
-  constructor() {}
+  
+  rotateVar: number;
+
+  constructor() {
+    this.rotateVar = 0;
+  }
+
+  
+
+  handleButtonClick(){
+    console.log("Button clicked");
+    Motion.addListener('orientation', event => {
+      this.updateArrowPosition(event.alpha);
+      console.log("Testing:", event)
+      console.log("Alpha", event.alpha);
+      console.log("Gamma", event.gamma);
+      console.log("Beta", event.gamma);
+    });
+  }
+
+  updateArrowPosition(zHeading: number){
+    this.rotateVar = -1 * zHeading;
+  }
+
+  rotateCheck(){
+    console.log("rotate fired");
+    this.rotateVar = Math.random() * 100;
+    console.log(this.rotateVar);
+  }
+
+
+
+  stopListeningClick(){
+    Motion.removeAllListeners();
+  }
+
+  private async handleButton2Click(){
+      try {
+        
+      } catch (e) {
+        // Handle error
+        return;
+      }
+    
+      // Once the user approves, can start listening:
+      
+};
+
+// Stop the acceleration listener
+stopAcceleration = () => {
+  
+};
+
+// Remove all listeners
+removeListeners = () => {
+  Motion.removeAllListeners();
+};
+
 }
