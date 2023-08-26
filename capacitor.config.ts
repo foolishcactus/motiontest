73a729b97e87c910de5d1884a6add5ac2d3362eb
Eshaan Gumudavelli <egumudavelli@gmail.com>
2023-08-26 11:41:01 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'motiontest',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
